3bdece60df96c379c6b692c0c0f0e165e54adaf8
James Hall <greatwolf3d@gmail.com>
2013-08-07 16:24:29 -0700

    Created AlertViewController

--- #LOSAL/#LOSAL/LAAlertsViewController.m
+++ #LOSAL/#LOSAL/LAAlertsViewController.m
@@ -8,30 +8,50 @@
 
 #import "LAAlertsViewController.h"
 
-@interface LAAlertsViewController ()
+#import "LAStoreManager.h"
 
+@interface LAAlertsViewController ()
+{
+     NSMutableArray *_objects;
+}
+
+@property (strong, nonatomic) LAStoreManager *storeManager;
 @end
 
 @implementation LAAlertsViewController
 
-- (id)initWithStyle:(UITableViewStyle)style
-{
-    self = [super initWithStyle:style];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
- 
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+	// Do any additional setup after loading the view, typically from a nib.
+    
+    //grabbing left bar button from libary
+    //    UIButton *menuBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    //    menuBtn.frame = CGRectMake(10, 10, 30, 21);
+    //    [menuBtn setBackgroundImage:[UIImage imageNamed:@"menuBtn.png"] forState:UIControlStateNormal];
+    //    [menuBtn addTarget:self action:@selector(revealMenu:) forControlEvents:UIControlEventTouchUpInside];
+    UIBarButtonItem *menuButton = [[UIBarButtonItem alloc] initWithTitle:@"Menu" style:UIBarButtonItemStyleBordered target:self action:@selector(revealMenu:)];
+    
+    self.navigationItem.leftBarButtonItem = menuButton;
+    
+    //    UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
+    //    self.navigationItem.rightBarButtonItem = addButton;
+    
+    // shadowPath, shadowOffset, and rotation is handled by ECSlidingViewController.
+    // You just need to set the opacity, radius, and color.
+    self.view.layer.shadowOpacity = 0.75f;
+    self.view.layer.shadowRadius = 10.0f;
+    self.view.layer.shadowColor = [UIColor blackColor].CGColor;
+    
+    if (![self.slidingViewController.underLeftViewController isKindOfClass:[LAMenuViewController class]]) {
+        self.slidingViewController.underLeftViewController  = [self.storyboard instantiateViewControllerWithIdentifier:@"Menu"];
+    }
+    
+    // This was messing up the scrolling in the UI table view so need to figure out a way to add this back - Joaquin
+    //[self.view addGestureRecognizer:self.slidingViewController.panGesture];
+    
+    [self fetchEntries];
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -39,83 +59,49 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-#pragma mark - Table view data source
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+-(void)fetchEntries
 {
-#warning Potentially incomplete method implementation.
-    // Return the number of sections.
-    return 2;
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-#warning Incomplete method implementation.
-    // Return the number of rows in the section.
-    return 2;
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    static NSString *CellIdentifier = @"Cell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
     
-    // Configure the cell...
+    UIView *currentTitleView = [[self navigationItem] titleView];
     
-    return cell;
-}
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-#pragma mark - Table view delegate
-
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Navigation logic may go here. Create and push another view controller.
-    /*
-     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
-     // ...
-     // Pass the selected object to the new view controller.
-     [self.navigationController pushViewController:detailViewController animated:YES];
-     */
+    UIActivityIndicatorView *aiView = [[UIActivityIndicatorView alloc]
+                                       initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhite];
+    [[self navigationItem] setTitleView:aiView];
+    
+    [aiView startAnimating];
+    
+    // This is where you populate the table with data
+    self.storeManager = [LAStoreManager sharedManager];
+    
+    [[self navigationItem] setTitleView:currentTitleView];
+    
+    [self.storeManager getFeedWithCompletion:^(NSArray *alertMessages, NSError *error)
+     {
+         NSLog(@"Completion block called!");
+         if (!error)
+         {
+             _objects = [NSMutableArray arrayWithArray:alertMessages];
+             [[self tableView] reloadData];
+             NSLog(@"error is %@", error);
+             
+         } else {
+             // If things went bad, show an alert view
+             NSString *errorString = [NSString stringWithFormat:@"Fetch failed: %@",
+                                      [error localizedDescription]];
+             
+             // Create and show an alert view with this error displayed
+             UIAlertView *av = [[UIAlertView alloc] initWithTitle:@"Error"
+                                                          message:errorString
+                                                         delegate:nil
+                                                cancelButtonTitle:@"OK"
+                                                otherButtonTitles:nil];
+             [av show];
+             // If you come here you got the array
+             NSLog(@"results are %@", alertMessages);
+         }
+     }];
+    
+    
 }
 
 @end
